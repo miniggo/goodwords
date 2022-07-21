9431008856c190900cc87a70a435e89e16fd73ba
miniggo <57436081+miniggo@users.noreply.github.com>
2022-07-21 13:47:46 +0900

    Add files via upload
    init commit

---
+++ src/main/java/com/ingstarneverland/goodwords/GoodwordsApplication.java
@@ -0,0 +1,13 @@
+package com.ingstarneverland.goodwords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodwordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodwordsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ingstarneverland/goodwords/GoodwordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingstarneverland.goodwords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodwordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
